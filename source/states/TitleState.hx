5d81bf07ad9bc926907bfc356e49605d695fe20f
CharlesCatYT <sptubea@gmail.com>
2023-11-08 21:18:40 -0500

    foxa is vibing!

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -231,12 +231,9 @@ class TitleState extends MusicBeatState
 			#end
 
 			default:
-			//EDIT THIS ONE IF YOU'RE MAKING A SOURCE CODE MOD!!!!
-			//EDIT THIS ONE IF YOU'RE MAKING A SOURCE CODE MOD!!!!
-			//EDIT THIS ONE IF YOU'RE MAKING A SOURCE CODE MOD!!!!
-				gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
-				gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
-				gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
+				gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle'); //foxa is vibing! :D
+				gfDance.animation.addByPrefix('danceLeft', 'dance', 24, false);
+				gfDance.animation.addByPrefix('danceRight', 'dance', 24, false);
 		}
 
 		add(gfDance);
