2f5161fbe5ccf31b0761f848148ccc2fc5ff6ca8
CharlesCatYT <sptubea@gmail.com>
2023-12-29 23:07:09 -0500

    Update GameOverSubstate.hx

--- source/substates/GameOverSubstate.hx
+++ source/substates/GameOverSubstate.hx
@@ -112,10 +112,7 @@ class GameOverSubstate extends MusicBeatSubstate
 						var exclude:Array<Int> = [];
 						//if(!ClientPrefs.cursing) exclude = [1, 3, 8, 13, 17, 21];
 						FlxG.sound.play(Paths.sound('jeffGameover/jeffGameover-' + FlxG.random.int(1, 25, exclude)), 1, false, null, true, function() {
-							if(!isEnding)
-							{
-								FlxG.sound.music.fadeIn(0.2, 1, 4);
-							}
+							if(!isEnding) FlxG.sound.music.fadeIn(4, 0.2, 1);
 						});
 					}
 					else coolStartDeath();
