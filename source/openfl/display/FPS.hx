86b7cf8b513fee9e94971aa3b4ed272f3af35812
CharlesCatYT <62410083+CharlesCatYT@users.noreply.github.com>
2024-02-09 22:14:16 -0500

    im grounded now, have the border shits for now!
    ok i will keep working when i uhhhhh 
    
    actually no this mod's left to die

--- source/openfl/display/FPS.hx
+++ source/openfl/display/FPS.hx
@@ -5,16 +5,19 @@ import openfl.events.Event;
 import openfl.text.TextField;
 import openfl.text.TextFormat;
 import flixel.math.FlxMath;
-#if gl_stats
-import openfl.display._internal.stats.Context3DStats;
-import openfl.display._internal.stats.DrawCallContext;
-#end
 #if flash
 import openfl.Lib;
 #end
 #if openfl
 import openfl.system.System;
 #end
+import haxe.Int64;
+import openfl.display.BlendMode;
+#if (gl_stats && !disable_cffi && (!html5 || !canvas))
+import openfl.display._internal.stats.Context3DStats;
+import openfl.display._internal.stats.DrawCallContext;
+#end
+
 /**
 	The FPS class provides an easy-to-use monitor to display
 	the current frame rate of an OpenFL project
@@ -36,11 +39,28 @@ class FPS extends TextField
 	public var memoryMegas:Float = 0;
 	public var memoryTotal:Float = 0;
 
+	public var borderSize:Int = 1;
+
+	// border code by @raltyro and @sayofthelor, taken from psike engine mcuz i 
+	@:noCompletion private final borders:Array<TextField> = new Array<TextField>();
+
 	@:noCompletion private var currentTime:Float;
 	@:noCompletion private var times:Array<Float>;
 	
 	public function new(x:Float = 10, y:Float = 10, color:Int = 0x000000) {
 		super();
+
+		var border:TextField;
+		for (i in 0...8) {
+			borders.push(border = new TextField());
+			border.selectable = false;
+			border.mouseEnabled = false;
+			border.autoSize = LEFT;
+			border.multiline = true;
+			border.width = 800;
+			border.height = 70;
+		}
+
 		this.x = x;
 		this.y = y;
 		currentFPS = 0;
@@ -50,6 +70,8 @@ class FPS extends TextField
 		autoSize = LEFT;
 		multiline = true;
 		text = "FPS: ";
+		width = 800;
+		height = 70;
 
 		currentTime = 0;
 		times = [];
@@ -60,8 +82,43 @@ class FPS extends TextField
 			__enterFrame(time - currentTime);
 		});
 		#end
+		addEventListener(Event.REMOVED, function(_) {
+			for (border in borders) this.parent.removeChild(border);
+		});
+		addEventListener(Event.ADDED, function(_) {
+			for (border in borders) this.parent.addChildAt(border, this.parent.getChildIndex(this));
+		});
 	}
 
+	@:noCompletion override function set_visible(value:Bool):Bool {
+		for (border in borders) border.visible = value;
+		return super.set_visible(value);
+	}
+
+	@:noCompletion override function set_defaultTextFormat(value:TextFormat):TextFormat {
+		for (border in borders) {
+			border.defaultTextFormat = value;
+			border.textColor = 0xFF000000;
+		}
+		return super.set_defaultTextFormat(value);
+	}
+
+	@:noCompletion override function set_x(x:Float):Float {
+		for (i in 0...8) borders[i].x = x + ([0, 3, 5].contains(i) ? borderSize : [2, 4, 7].contains(i) ? -borderSize : 0);
+		return super.set_x(x);
+	}
+
+	@:noCompletion override function set_y(y:Float):Float {
+		for (i in 0...8) borders[i].y = y + ([0, 1, 2].contains(i) ? borderSize : [5, 6, 7].contains(i) ? -borderSize : 0);
+		return super.set_y(y);
+	}
+
+	@:noCompletion override function set_text(text:String):String {
+		for (border in borders) border.text = text;
+		return super.set_text(text);
+	}
+
+	
 	var deltaTimeout:Float = 0.0;
 
 	// Event Handlers
@@ -122,3 +179,5 @@ class FPS extends TextField
 			text += "\n";
 		}
 }
+
+
