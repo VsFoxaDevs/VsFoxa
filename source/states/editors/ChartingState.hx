13a83c5225f1ac4414de3e68d0a865264799b736
CharlesCatYT <sptubea@gmail.com>
2024-02-17 22:13:44 -0500

    switch to our own forks :)))

--- source/states/editors/ChartingState.hx
+++ source/states/editors/ChartingState.hx
@@ -45,11 +45,7 @@ import flixel.util.FlxStringUtil;
 import flash.media.Sound;
 #end
 
-#if (flixel < "5.3.0")
-@:access(flixel.system.FlxSound._sound)
-#else
 @:access(flixel.sound.FlxSound._sound)
-#end
 @:access(openfl.media.Sound.__buffer)
 
 class ChartingState extends MusicBeatState
